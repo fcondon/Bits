4d81684967f813e3790cfb3a5092ee8fe8f44089
Fiona Condon <frcondon@gmail.com>
2015-09-02 01:33:24 -0400

    Adding a couple arithmetic operators to lambda calculus workspace

--- lambchop_calculus.js
+++ lambchop_calculus.js
@@ -6,6 +6,7 @@ var SUCC = λ("n:λ(f:λ(x:f(n(f)(x))))");
 var ZERO = λ("f:λ(x:x)"); 
 var ONE = SUCC(ZERO);
 var TWO = SUCC(ONE);
+var THREE = SUCC(TWO);
 
 var IF = λ("f:λ(t:λ(e:(f(t)(e))))"); 
 var _IF = function(c) {
@@ -23,13 +24,26 @@ var _TRUE = function(t) {
     };
 };
 
-var _FALSE = λ("t:λ(e:e)");
-var FALSE = function(t) {
+var FALSE = λ("t:λ(e:e)");
+var _FALSE = function(t) {
     return function(e) {
         return e;
     };
 };
 
-var result = IF(TRUE)(ZERO)(ONE);
+// λm:λ(n:m(SUCC)(n))
+var ADD = λ("m:λ(n:λ(f:λ(x:((n(f))((m(f))(x))))))");
+var _ADD = function(m) {
+    return function(n) {
+        return m(SUCC)(n)
+    };
+}
 
-process.stdout.write(NUM(result) + "\n");
+var TIMES = λ("x:λ(y:λ(z:x(y(z))))");
+var _TIMES = function(x) {
+    return function(y) {
+        return function(z) {
+            return x(y(z));
+        };
+    };
+}
